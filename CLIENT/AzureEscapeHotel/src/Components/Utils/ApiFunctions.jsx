3cbd63c1b8c857a1d6ca906938aa0b9c587f5dcd
Cauã Sebastian <cauasebastian2207@gmail.com>
2024-05-18 18:48:57 -0300

    .

--- CLIENT/AzureEscapeHotel/src/Components/Utils/ApiFunctions.jsx
+++ CLIENT/AzureEscapeHotel/src/Components/Utils/ApiFunctions.jsx
@@ -1,29 +1,42 @@
-import axios from "axios"
+import axios from 'axios';
 
 export const api = axios.create({
-    baseURL : "http://localhost:9192"
-})
-//this function add new rooms in your database
-export async function addRoom(photo,roomType,roomPrice){
-    const formData = new FormData()
-    formData.append("photo",photo)
-    formData.append("roomType",roomType)
-    formData.append("roomPrice",roomPrice)
+    baseURL: 'http://localhost:9192',
+});
 
-    const response = await api.post("/rooms/add/new-room", formData)
-    if(response.status === 201){
-        return true
-    }else{
-        return false
+export async function addRoom(photo, roomType, roomPrice) {
+    const formData = new FormData();
+    formData.append('photo', photo);
+    formData.append('roomType', roomType);
+    formData.append('roomPrice', roomPrice);
+
+    const response = await api.post('/rooms/add/new-room', formData);
+    return response.status === 201;
+}
+
+export async function getRoomTypes() {
+    try {
+        const response = await api.get('/rooms/room-types');
+        return response.data;
+    } catch (error) {
+        throw new Error('Error fetching room types');
     }
 }
-//this func get all room types in your database
-export async function getRoomTypes(){
-    try{
-    const response = await api.get("/rooms/room-types")
-    return response.data
+
+export async function getAllRooms() {
+    try {
+        const response = await api.get('/rooms/all-rooms');
+        return response.data;
+    } catch (error) {
+        throw new Error('Error fetching rooms');
     }
-    catch(error){
-        throw new error("Error fetching room types")
+}
+
+export async function deleteRoom(roomId) {
+    try {
+        const response = await api.delete(`/rooms/delete/room/${roomId}`);
+        return response.data;
+    } catch (error) {
+        throw new Error('Error deleting room');
     }
 }
