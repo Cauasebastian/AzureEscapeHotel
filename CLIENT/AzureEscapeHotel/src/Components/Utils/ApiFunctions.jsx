b05446a21e56e79f4a1af22570614fb47849352f
Laranunes1 <133614515+Cauasebastian@users.noreply.github.com>
2024-07-11 03:57:37 -0300

    Update ApiFunctions.jsx

--- CLIENT/AzureEscapeHotel/src/Components/Utils/ApiFunctions.jsx
+++ CLIENT/AzureEscapeHotel/src/Components/Utils/ApiFunctions.jsx
@@ -75,3 +75,49 @@ export async function getRoomById(roomId){
         throw new Error('Error fetching room');
     }
 }
+export async function bookRoom(roomId, booking){
+    try{
+        const response = await api.post(`/bookings/room/${roomId}/booking`, booking);
+        return response.data;
+    }catch(error){
+        if(error.response && error.response.status === 409){
+            throw new Error(error.response.data);
+        }
+        else{
+            throw new Error(`error booking room: ${error.message}`); 
+        }
+    }
+}
+export async function getAllBookings(){
+    try{
+        const response = await api.get('/bookings/all-bookings');
+        return response.data;
+    }catch(error){
+        throw new Error(`error fetching bookings: ${error.message}`);
+    }
+}
+export async function getBookingByConfirmationCode(confirmationCode){
+    try{
+        const response = await api.get(`/bookings/confirmation/${confirmationCode}`);
+        return response.data;
+    }catch(error){
+        if(error.response && error.response.status === 404){
+            throw new Error(`Booking with confirmation code ${confirmationCode} not found`);
+        }
+        else{
+            throw new Error(`error fetching booking: ${error.message}`);
+        }
+    }
+}
+export async function cancelBooking(bookingId){
+    try{
+        const response = await api.delete(`/bookings/booking/${bookingId}/delete`);
+        return response.data;
+    }catch(error){
+        if(error.response && error.response.status === 404){
+            throw new Error(`Booking with ID ${bookingId} not found`);
+        }
+        else{
+            throw new Error(`error cancelling booking: ${error.message}`);
+        }}
+}
